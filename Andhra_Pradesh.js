f0cc1d71589d08dfff1da0b8748e79cb2066d896
pxthxk <5724479+pxthxk@users.noreply.github.com>
2021-04-27 12:43:04 +0530

    Update AP data

--- Andhra_Pradesh.js
+++ Andhra_Pradesh.js
@@ -13,8 +13,9 @@ class Andhra_Pradesh {
         var oxy_vac = "Available: " + andhra_pradesh_data[i]["OXYGEN_GENERAL_AVAILABLE"] + "<br><br>";
         var gen_occ = "<b>GENERAL BEDS</b><br>Total: " + andhra_pradesh_data[i]["GENERAL_TOTAL"] + "<br>";
         var gen_vac = "Available: " + andhra_pradesh_data[i]["GENERAL_AVAILABLE"] + "<br><br>";
+        var ven_tot = "<b>VENTILATORS</b><br>Total: " + andhra_pradesh_data[i]["VENTILATOR"] + "<br>";
         
-        var marker = L.marker(new L.LatLng(coord[0], coord[1])).bindPopup(hspInfo + contact + loc + icu_occ + icu_vac + oxy_occ + oxy_vac + gen_occ + gen_vac);
+        var marker = L.marker(new L.LatLng(coord[0], coord[1])).bindPopup(hspInfo + contact + loc + icu_occ + icu_vac + oxy_occ + oxy_vac + gen_occ + gen_vac + ven_tot);
         mcg.addLayer(marker);
       }
     }
