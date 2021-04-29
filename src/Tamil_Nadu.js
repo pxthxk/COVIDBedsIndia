73b8cb299915599a301a4605c1edb9aed938cee7
pxthxk <5724479+pxthxk@users.noreply.github.com>
2021-04-30 02:14:28 +0530

    Update TN to fetch from API

--- src/Tamil_Nadu.js
+++ src/Tamil_Nadu.js
@@ -1,44 +1,52 @@
+var tamil_nadu_data;
+
 class Tamil_Nadu {
   populate(mcg, bedtype) {
-    for(var i in tamil_nadu_data) {
-      var flag = 0;
+    let fetchPromise = fetch("https://api.covidbedsindia.in/v1/storages/6089820e03eef3b588d05a6d/Tamil%20Nadu");
+    
+    fetchPromise.then(response => {
+      return response.json();
+    }).then(tamil_nadu_data => {
+      tamil_nadu_data.map(i => {
+        var flag = 0;
 
-      try {
-        if((tamil_nadu_data[i]["LAT"] != "") && (tamil_nadu_data[i]["LONG"] != "")) {
-          if(bedtype == "icu") {
-            if(tamil_nadu_data[i]["HAS_ICU_BEDS"] == "TRUE" || tamil_nadu_data[i]["HAS_VENTILATORS"] == "TRUE") {
+        try {
+          if((i["LAT"] != "") && (i["LONG"] != "")) {
+            if(bedtype == "icu") {
+              if(tamil_nadu_data[i]["HAS_ICU_BEDS"] == "TRUE" || i["HAS_VENTILATORS"] == "TRUE") {
+                flag = 1;
+              }
+            } else if(bedtype == "ventilator") {
+              if(i["HAS_VENTILATORS"] == "TRUE") {
+                flag = 1;
+              }
+            } else {
               flag = 1;
             }
-          } else if(bedtype == "ventilator") {
-            if(tamil_nadu_data[i]["HAS_VENTILATORS"] == "TRUE") {
-              flag = 1;
+
+            if(flag == 1) {
+              var coord = [i["LAT"], i["LONG"]];
+
+              var hspInfo = "<b>" + i["HOSPITAL_NAME"] + "</b><br>";
+              var contact = "Contact: " + i["CONTACT"] + "<br>";
+              var loc = "<a href=" + i["LOCATION"] + " target='_blank'>Click for Google Maps</a><br><br>";
+              var sus_tot = "<b>BEDS FOR SUSPECTED CASES</b><br>Total: " + i["BEDS_FOR_SUSPECTED_CASES_TOTAL"] + "<br>";
+              var sus_vac = "Available: " + i["BEDS_FOR_SUSPECTED_CASES_VACANT"] + "<br><br>";
+              var oxy_tot = "<b>OXYGEN BEDS</b><br>Total: " + i["OXYGEN_SUPPORTED_BEDS_TOTAL"] + "<br>";
+              var oxy_vac = "Available: " + i["OXYGEN_SUPPORTED_BEDS_VACANT"] + "<br><br>";
+              var nonoxy_tot = "<b>NON-OXYGEN BEDS</b><br>Total: " + i["NONOXYGEN_SUPPORTED_BEDS_TOTAL"] + "<br>";
+              var nonoxy_vac = "Available: " + i["NONOXYGEN_SUPPORTED_BEDS_VACANT"] + "<br><br>";
+              var icu_tot = "<b>ICU BEDS</b><br>Total: " + i["ICU_BEDS_TOTAL"] + "<br>";
+              var icu_vac = "Available: " + i["ICU_BEDS_VACANT"] + "<br><br>";
+              
+              var marker = L.marker(new L.LatLng(coord[0], coord[1])).bindPopup(hspInfo + contact + loc + sus_tot + sus_vac + oxy_tot + oxy_vac + nonoxy_tot + nonoxy_vac + icu_tot + icu_vac);
+              mcg.addLayer(marker);
             }
-          } else {
-            flag = 1;
           }
-
-          if(flag == 1) {
-            var coord = [tamil_nadu_data[i]["LAT"], tamil_nadu_data[i]["LONG"]];
-
-            var hspInfo = "<b>" + tamil_nadu_data[i]["HOSPITAL_NAME"] + "</b><br>";
-            var contact = "Contact: " + tamil_nadu_data[i]["CONTACT"] + "<br>";
-            var loc = "<a href=" + tamil_nadu_data[i]["LOCATION"] + " target='_blank'>Click for Google Maps</a><br><br>";
-            var sus_tot = "<b>BEDS FOR SUSPECTED CASES</b><br>Total: " + tamil_nadu_data[i]["BEDS_FOR_SUSPECTED_CASES_TOTAL"] + "<br>";
-            var sus_vac = "Available: " + tamil_nadu_data[i]["BEDS_FOR_SUSPECTED_CASES_VACANT"] + "<br><br>";
-            var oxy_tot = "<b>OXYGEN BEDS</b><br>Total: " + tamil_nadu_data[i]["OXYGEN_SUPPORTED_BEDS_TOTAL"] + "<br>";
-            var oxy_vac = "Available: " + tamil_nadu_data[i]["OXYGEN_SUPPORTED_BEDS_VACANT"] + "<br><br>";
-            var nonoxy_tot = "<b>NON-OXYGEN BEDS</b><br>Total: " + tamil_nadu_data[i]["NONOXYGEN_SUPPORTED_BEDS_TOTAL"] + "<br>";
-            var nonoxy_vac = "Available: " + tamil_nadu_data[i]["NONOXYGEN_SUPPORTED_BEDS_VACANT"] + "<br><br>";
-            var icu_tot = "<b>ICU BEDS</b><br>Total: " + tamil_nadu_data[i]["ICU_BEDS_TOTAL"] + "<br>";
-            var icu_vac = "Available: " + tamil_nadu_data[i]["ICU_BEDS_VACANT"] + "<br><br>";
-            
-            var marker = L.marker(new L.LatLng(coord[0], coord[1])).bindPopup(hspInfo + contact + loc + sus_tot + sus_vac + oxy_tot + oxy_vac + nonoxy_tot + nonoxy_vac + icu_tot + icu_vac);
-            mcg.addLayer(marker);
-          }
+        } catch(err) {
+          console.log(err);
         }
-      } catch(err) {
-        console.log(err);
-      }
-    }
+      });
+    });
   }
 }
