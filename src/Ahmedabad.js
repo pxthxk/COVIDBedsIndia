2ebf78308c4828e5f2198e97941f844e263361db
pxthxk <5724479+pxthxk@users.noreply.github.com>
2021-05-02 23:24:08 +0530

    Update Ahmedabad data

--- src/Ahmedabad.js
+++ src/Ahmedabad.js
@@ -29,17 +29,24 @@ class Ahmedabad {
             var contact = "Contact: " + ahmedabad_data[i]["CONTACT"] + "<br>";
             var loc = "<a href=" + ahmedabad_data[i]["LOCATION"] + " target='_blank'>View on Google Maps</a><br>";
             var last_updated_at = "Last Updated: " + ahmedabad_data[i]["LAST_UPDATED"] + "<br><br>";
-            var type = ahmedabad_data[i]["TYPE"] + "<br><br>";
-            var iso_occ = "<b>ISOLATION BEDS</b><br>Occupied: " + ahmedabad_data[i]["ISOLATION_OCCUPIED"] + "<br>";
-            var iso_vac = "Available: " + ahmedabad_data[i]["ISOLATION_VACANT"] + "<br><br>";
-            var hdu_occ = "<b>HIGH DEPENDENCY UNIT BEDS</b><br>Occupied: " + ahmedabad_data[i]["HIGH_DEPENDENCY_UNIT_OCCUPIED"] + "<br>";
-            var hdu_vac = "Available: " + ahmedabad_data[i]["HIGH_DEPENDENCY_UNIT_VACANT"] + "<br><br>";
-            var icu_occ = "<b>ICU BEDS</b><br>Occupied: " + ahmedabad_data[i]["ICU_WITHOUT_VENTILATOR_OCCUPIED"] + "<br>";
-            var icu_vac = "Available: " + ahmedabad_data[i]["ICU_WITHOUT_VENTILATOR_VACANT"] + "<br><br>";
-            var ven_occ = "<b>ICU BEDS WITH VENTILATOR</b><br>Occupied: " + ahmedabad_data[i]["ICU_WITH_VENTILATOR_OCCUPIED"] + "<br>";
-            var ven_vac = "Available: " + ahmedabad_data[i]["ICU_WITH_VENTILATOR_VACANT"] + "<br><br>";
+            var amc_iso_occ = "<b>AMC QUOTA FOR PRIVATE PATIENTS</b><br><u>ISOLATION BEDS</u><br>Occupied: " + ahmedabad_data[i]["AMC_ISOLATION_OCCUPIED"] + " ";
+            var amc_iso_vac = "Available: " + ahmedabad_data[i]["AMC_ISOLATION_VACANT"] + "<br><br>";
+            var amc_hdu_occ = "<u>HIGH DEPENDENCY UNIT BEDS</u><br>Occupied: " + ahmedabad_data[i]["AMC_HIGH_DEPENDENCY_UNIT_OCCUPIED"] + " ";
+            var amc_hdu_vac = "Available: " + ahmedabad_data[i]["AMC_HIGH_DEPENDENCY_UNIT_VACANT"] + "<br><br>";
+            var amc_icu_occ = "<u>ICU BEDS</u><br>Occupied: " + ahmedabad_data[i]["AMC_ICU_WITHOUT_VENTILATOR_OCCUPIED"] + " ";
+            var amc_icu_vac = "Available: " + ahmedabad_data[i]["AMC_ICU_WITHOUT_VENTILATOR_VACANT"] + "<br><br>";
+            var amc_ven_occ = "<u>ICU BEDS WITH VENTILATOR</u><br>Occupied: " + ahmedabad_data[i]["AMC_ICU_WITH_VENTILATOR_OCCUPIED"] + " ";
+            var amc_ven_vac = "Available: " + ahmedabad_data[i]["AMC_ICU_WITH_VENTILATOR_VACANT"] + "<br><br>";
+            var private_iso_occ = "<b>PRIVATE QUOTA FOR PRIVATE PATIENTS</b><br><u>ISOLATION BEDS</u><br>Occupied: " + ahmedabad_data[i]["PRIVATE_ISOLATION_OCCUPIED"] + " ";
+            var private_iso_vac = "Available: " + ahmedabad_data[i]["PRIVATE_ISOLATION_VACANT"] + "<br><br>";
+            var private_hdu_occ = "<u>HIGH DEPENDENCY UNIT BEDS</u><br>Occupied: " + ahmedabad_data[i]["PRIVATE_HIGH_DEPENDENCY_UNIT_OCCUPIED"] + " ";
+            var private_hdu_vac = "Available: " + ahmedabad_data[i]["PRIVATE_HIGH_DEPENDENCY_UNIT_VACANT"] + "<br><br>";
+            var private_icu_occ = "<u>ICU BEDS</u><br>Occupied: " + ahmedabad_data[i]["PRIVATE_ICU_WITHOUT_VENTILATOR_OCCUPIED"] + " ";
+            var private_icu_vac = "Available: " + ahmedabad_data[i]["PRIVATE_ICU_WITHOUT_VENTILATOR_VACANT"] + "<br><br>";
+            var private_ven_occ = "<u>ICU BEDS WITH VENTILATOR</u><br>Occupied: " + ahmedabad_data[i]["PRIVATE_ICU_WITH_VENTILATOR_OCCUPIED"] + " ";
+            var private_ven_vac = "Available: " + ahmedabad_data[i]["PRIVATE_ICU_WITH_VENTILATOR_VACANT"] + "<br><br>";
 
-            var marker = L.marker(new L.LatLng(coord[0], coord[1])).bindPopup(hspInfo + zone + contact + loc + last_updated_at + type + iso_occ + iso_vac + hdu_occ + hdu_vac + icu_occ + icu_vac + ven_occ + ven_vac);
+            var marker = L.marker(new L.LatLng(coord[0], coord[1])).bindPopup(hspInfo + zone + (contact ? ahmedabad_data[i]["CONTACT"] : null) + loc + last_updated_at + amc_iso_occ + amc_iso_vac + amc_hdu_occ + amc_hdu_vac + amc_icu_occ + amc_icu_vac + amc_ven_occ + amc_ven_vac + private_iso_occ + private_iso_vac + private_hdu_occ + private_hdu_vac + private_icu_occ + private_icu_vac + private_ven_occ + private_ven_vac);
             mcg.addLayer(marker);
           }
         }
